c74af01080392cefb593942b2846196909527922
RichardMaS <richardluowhoo@gmail.com>
2022-07-22 14:37:34 -0400

    uganda

--- population_estimator.py
+++ population_estimator.py
@@ -27,7 +27,7 @@ dataset_name = 'Ethnologue Population Mapping'
 Task.add_requirements("-rrequirements.txt")
 task = Task.init(
   project_name='Ethnologue_Richard_Internship',    # project name of at least 3 characters
-  task_name='Guinea' + str(int(time.time())), # task name of at least 3 characters
+  task_name='Uganda' + str(int(time.time())), # task name of at least 3 characters
   task_type="training",
   tags=None,
   reuse_last_task_id=True,
@@ -44,8 +44,8 @@ fp = os.path.join(dataset_path, "Language Polygons/SIL_lang_polys_June2022.shp")
 data = gpd.read_file(fp)
 
 # CHANGE COUNTRY LABELS HERE
-ctry_name = "Guinea"
-ctry_abbr = "GIN"
+ctry_name = "Uganda"
+ctry_abbr = "UGA"
 
 grouped = data.groupby("COUNTRY_IS")
 ctry = grouped.get_group(ctry_abbr, data)
@@ -152,4 +152,4 @@ result.rename(columns={"ISO_LANGUA": "ISO_639",
 # result.to_csv(f"{ctry_name}_Population_Estimates.csv")
 
 # Save the artifact in ClearML
-task.upload_artifact(name='Guinea', artifact_object=result)
+task.upload_artifact(name='Uganda', artifact_object=result)
